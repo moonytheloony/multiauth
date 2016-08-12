ebdd50f63808dfc63659f523395721fe15b34a53
Rahul Rai <rahulrai@live.com>
2016-08-12 13:46:45 +1000

    Add project files.

---
+++ MultiAuthApp/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using MultiAuthApp.Models;
+
+namespace MultiAuthApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // To execute a policy, you simply need to trigger an OWIN challenge.
+                // You can indicate which policy to use by specifying the policy id as the AuthenticationType
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties() { RedirectUri = "/" }, Startup.SignInPolicyId);
+            }
+        }
+
+        public void SignUp()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties() { RedirectUri = "/" }, Startup.SignUpPolicyId);
+            }
+        }
+
+
+        public void Profile()
+        {
+            if (Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties() { RedirectUri = "/" }, Startup.ProfilePolicyId);
+            }
+        }
+
+        public void SignOut()
+        {
+            // To sign out the user, you should issue an OpenIDConnect sign out request.
+            if (Request.IsAuthenticated)
+            {
+                IEnumerable<AuthenticationDescription> authTypes = HttpContext.GetOwinContext().Authentication.GetAuthenticationTypes();
+                HttpContext.GetOwinContext().Authentication.SignOut(authTypes.Select(t => t.AuthenticationType).ToArray());
+                Request.GetOwinContext().Authentication.GetAuthenticationTypes();
+            }
+        }
+    }
+}
---
+++ MultiAuthApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiAuthApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // You can use the PolicyAuthorize decorator to execute a certain policy if the user is not already signed into the app.
+        [Authorize]
+        public ActionResult Claims()
+        {
+            Claim displayName = ClaimsPrincipal.Current.Identities.First().Claims.Where(claim => claim.Type == "emails").First();
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+            return View();
+        }
+
+        [Authorize(Roles = "Employee")]
+        public ActionResult EmployeeClaims()
+        {
+            Claim displayName = ClaimsPrincipal.Current.Identities.First().Claims.Where(claim => claim.Type == "emails").First();
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+            return View();
+        }
+
+        public ActionResult Error(string message)
+        {
+            ViewBag.Message = message;
+            return View("Error");
+        }
+    }
+}
